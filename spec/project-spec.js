87d0ea88b6c9e60b8ada5b4c096d8daa60f2762a
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-06-28 12:21:33 -0700

    Fixed testing to actually run, worked on testing backend logic

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -1,13 +1,17 @@
-import { XXX } from './../src/backend-code.js';
+import { UserBirth } from './../src/backend-code.js';
 
 describe('age-calc', function() {
 
   it('should save a date as a Date object', function() {
-    const inputDate = new Date (2009, 06, 27);
+    const inputDate = new Date (2009, 6, 27);
     expect(inputDate.getFullYear()).toEqual(2009);
-    expect(inputDate.getMonth()).toEqual(5);
+    expect(inputDate.getMonth()).toEqual(6);
     expect(inputDate.getDate()).toEqual(27);
   });
 
+  it('should calculate the age', function() {
+    const inputDate = new Date (2009, 6, 27);
+    expect(inputDate.getAge()).toEqual(10);
+  });
 
-})
+});
--- src/backend-code.js
+++ src/backend-code.js
@@ -1,19 +1,19 @@
-// export class UserBirth extends Date {
-//   constructor(year, month, date) {
-//     // super();
-//     this.year = year;
-//     this.month = month;
-//     this.date = date;
-//   }
-// }
+export class UserBirth extends Date {
+  constructor(year, month, date) {
+    super();
+    this.year = year;
+    this.month = month;
+    this.date = date;
+  }
 
-// const getAge = function(UserBirth) {
-//       var today = new Date();
-//       var birthDate = new Date(dateString);
-//       var age = today.getFullYear() - birthDate.getFullYear();
-//       var month = today.getMonth() - birthDate.getMonth();
-//       if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-//           age--;
-//       }
-//       return age;
-// }
+  const getAge = function(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+}
