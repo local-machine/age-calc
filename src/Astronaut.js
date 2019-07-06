1fa993181e37cd84048572115bced8ca35d4d5fa
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-07-06 12:20:34 -0700

    Edited README specs to be more accurate and added comments to astronaut.js

--- src/Astronaut.js
+++ src/Astronaut.js
@@ -11,32 +11,39 @@ export class Astronaut {
     this.age = age;
   }
 
+//calculate the age on Mercury
   mercuryAge() {
     return Math.floor(this.age / 0.24);
   }
 
+// calculate the age on Venus
   venusAge() {
     return Math.floor(this.age / 0.62);
   }
 
+// calculate the age on Earth
   earthAge() {
     return this.age;
   }
 
+// calculate the age on Mars
   marsAge() {
     return Math.floor(this.age / 1.88);
   }
 
+// calculate the age on Jupiter
   jupiterAge() {
     return Math.floor(this.age / 11.86);
   }
 
+// calculate the number of years before user meets average life expectancy
   yearsLeft() {
     if (this.age <= 71) {
       return (71 - this.age);
     }
   }
 
+// calculate the number of years past average life expectancy
   yearsPast() {
     if (this.age > 71) {
       return (this.age - 71);
